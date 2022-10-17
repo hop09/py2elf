aa97d88e43772c132fc42b3c6b7fa870f6a9520d
Muhammad Hamza <communityn99@gmail.com>
2022-10-17 10:34:52 +0500

    Update py2elf.py

--- py2elf.py
+++ py2elf.py
@@ -6,6 +6,6 @@ ex = file.split('.')[0]
 build(file)
 
 os.system('strip -s '+ex)
-os.systen('termux-elf-cleaner '+ex)
+os.system('termux-elf-cleaner '+ex)
 os.system(f'rm -rf {ex}.c {ex}.o && mv {ex} /sdcard')
 print(' File stored in /sdcard/'+ex)

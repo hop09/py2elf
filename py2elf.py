0c9f945300c71016fb243ed7bc1b9ad8e80abeed
Muhammad Hamza <communityn99@gmail.com>
2022-10-17 10:34:36 +0500

    Create py2elf.py

---
+++ py2elf.py
@@ -0,0 +1,11 @@
+import os
+from Cython.Build.BuildExecutable import build
+file = input(' Put file:  ')
+
+ex = file.split('.')[0]
+build(file)
+
+os.system('strip -s '+ex)
+os.systen('termux-elf-cleaner '+ex)
+os.system(f'rm -rf {ex}.c {ex}.o && mv {ex} /sdcard')
+print(' File stored in /sdcard/'+ex)
